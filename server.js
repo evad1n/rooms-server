7312dd41cd669685a4272ae0b4869af2bb92cbad
evad1n <willdickinson98@gmail.com>
2019-07-08 12:59:08 -0600

    stuff look good

--- server.js
+++ server.js
@@ -11,58 +11,91 @@ server.use(cors());
 server.use(express.json());
 server.use(express.urlencoded({ extended: false }));
 
+// MESSAGING
+var messageHistory = []
 
-var history = []
-var position = {
-    "x": 250,
-    "y": 250
-}
+// GAME
+var gameMessageHistory = []
+var characters = []
+var characterCount = 0
 
 //normal functions :)
 server.get("/messaging", function (req, res) {
     res.send({
-        history: history,
-        position: position
+        history: messageHistory,
     })
 });
 
 server.post("/messaging", function (req, res) {
-    history.push(req.body.message)
-    res.send(history)
+    messageHistory.push(req.body.message)
+    res.send(messageHistory)
 });
 
-server.post("/game", function (req, res) {
-    if(req.body.move == "left"){
-        position.x -= 10
-    } else {
-        position.x += 10
+server.get("/game", function (req, res) {
+    res.send({
+        history: gameMessageHistory,
+        characters: characters
+    })
+});
+
+server.post("/game/login", function (req, res) {
+    newPlayer = {
+        id: characterCount,
+        position: {
+            "x": 250,
+            "y": 250
+        },
+        color: "red"
     }
-    res.send(position)
+    console.log(newPlayer.id)
+    characterCount++
+    console.log(newPlayer.id)
+    characters.push(newPlayer)    
+    res.send({
+        id: newPlayer.id
+    })
 });
 
-// server.delete("/posts/:id", function (req, res) {
-//     postsModel.findByIdAndDelete(req.params.id).then(function () {
-//         res.status(204)
-//         res.send()
-//     }).catch(function (error) {
-//         res.status(400)
-//         res.json({
-//             msg: error.message
-//         })
-//     })
-// })
+server.post("/game/:id", function (req, res) {
+    var player
+    characters.forEach(el => {
+        if (el.id == req.params.id)
+            player = el
+    });
 
-// server.put('/posts/:id', function (req, res) {
-//     postsModel.findByIdAndUpdate(req.params.id, req.body).then(function () {
-//         res.status(204)
-//         res.send()
-//     }).catch(function (error) {
-//         res.status(400)
-//         res.json({
-//             msg: error.message
-//         })
-//     })
-// })
+    if(req.body.move == "left"){
+        player.position.x -= 10
+    } else if (req.body.move == "right") {
+        player.position.x += 10
+    } else if (req.body.move == "up") {
+        player.position.y -= 10
+    } else if (req.body.move == "down") {
+        player.position.y += 10
+    }
+    res.send(player.position)
+});
+
+server.post("/game/messages/:id", function (req, res) {
+    gameMessageHistory.push(req.body.message)
+    res.send(gameMessageHistory)
+});
+
+server.post("/game/color/:id", function (req, res) {
+    gameMessageHistory.push(req.body.message)
+    res.send(gameMessageHistory)
+});
+
+server.delete("/game/:id", function (req, res) {
+    console.log("deleted")
+    var player
+    characters.forEach(el => {
+        if (el.id == req.params.id)
+            player = el
+    });
+    var index = characters.indexOf(player)
+    characters.splice(index, 1)
+    res.send()
+})
 
 server.listen(port, function () {
     console.log("Listening on " + port);
