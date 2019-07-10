518695e15daad1c061c36455fed1061681e79bc0
evad1n <willdickinson98@gmail.com>
2019-07-10 10:00:06 -0600

    global and local users working

--- server.js
+++ server.js
@@ -11,66 +11,86 @@ server.use(cors());
 server.use(express.json());
 server.use(express.urlencoded({ extended: false }));
 
-//Global session info
-var users = []
+// Global session info
+// array of rooms, each room is an object containing array of users, array of messageHistory, and specific room data
+var rooms = []
+// do this for each room
+rooms["globalMessaging"] = {
+    users: [],
+    messageHistory: [],
+}
+rooms["otherMessaging"] = {
+    users: [],
+    messageHistory: [],
+}
+rooms["game"] = {
+    users: [],
+    messageHistory: [],
+    characters: []
+}
+var globalUsers = []
 
-// MESSAGING
-var mainMessageHistory = []
-var sideMessageHistory = []
-
-// GAME
-var gameMessageHistory = []
-var characters = []
-
+//start server
+server.listen(port, function () {
+    console.log("Listening on " + port);
+})
 
 //check usernames
 server.get("/users", function (req, res) {
     res.send({
-        users: users
+        users: globalUsers
     })
 })
 
 //add username
 server.post("/users", function (req, res) {
-    users.push(req.body.username)
-    console.log(req.body.username)
-    res.send(users)
+    globalUsers.push(req.body.username)
+    console.log(req.body.username + " has logged in")
+    res.send(globalUsers)
 })
 
-server.delete("/:user", function (req, res) {
-    console.log("deleted ", req.params.user)
+//delete user when page is closed
+server.delete("/users", function (req, res) {
+    console.log(req.body.user + " has logged off")
     var tmp
-    users.forEach(user => {
-        if (user.id == req.params.user)
+    globalUsers.forEach(user => {
+        if (user.id == req.body.user)
             tmp = user
     });
-    var index = users.indexOf(tmp)
-    users.splice(index, 1)
-    res.send()
+    var index = globalUsers.indexOf(tmp)
+    globalUsers.splice(index, 1)
+    res.send(globalUsers)
 })
 
-//normal functions :)
-server.get("/messaging/main", function (req, res) {
-    res.send({
-        history: mainMessageHistory,
-    })
+//global messaging that goes to room based on parameter
+server.get("/:room/messaging", function (req, res) {
+    res.send({ history: rooms[req.params.room].messageHistory, users: rooms[req.params.room].users })
 });
 
-server.post("/messaging/main", function (req, res) {
-    mainMessageHistory.push(req.body.message)
-    res.send(mainMessageHistory)
+server.post("/:room/messaging", function (req, res) {
+    rooms[req.params.room].messageHistory.push(req.body.message)
+    res.send( rooms[req.params.room].messageHistory)
 });
 
-server.get("/messaging/side", function (req, res) {
-    res.send({
-        history: sideMessageHistory,
-    })
-});
+// get all users in room
+server.get("/:room/users", function (req, res) {
+    res.send(rooms[req.params.room].users)
+})
 
-server.post("/messaging/side", function (req, res) {
-    sideMessageHistory.push(req.body.message)
-    res.send(sideMessageHistory)
-});
+// add user to room
+server.post("/:room/users", function (req, res) {
+    rooms[req.params.room].users.push(req.body.user)
+    console.log(req.body.user + " has joined " + req.params.room)
+    res.send(rooms[req.params.room].users)
+})
+
+// remove user from room
+server.put('/:room/users', function (req, res) {
+    var index = rooms[req.params.room].users.indexOf(req.body.user)
+    rooms[req.params.room].users.splice(index, 1)
+    console.log(req.body.user + " has left " + req.params.room)
+    res.send(rooms[req.params.room].users)
+})
 
 server.get("/game", function (req, res) {
     res.send({
@@ -86,7 +106,7 @@ server.post("/game/login", function (req, res) {
             "x": 250,
             "y": 250
         },
-        color: "red"
+        color: req.body.color
     }
     characters.push(newPlayer)
     console.log(newPlayer)
@@ -113,18 +133,3 @@ server.post("/game", function (req, res) {
     }
     res.send(player.position)
 });
-
-server.get("/game/messaging", function (req, res) {
-    res.send({
-        history: gameMessageHistory,
-    })
-});
-
-server.post("/game/messaging", function (req, res) {
-    gameMessageHistory.push(req.body.message)
-    res.send(gameMessageHistory)
-});
-
-server.listen(port, function () {
-    console.log("Listening on " + port);
-})
